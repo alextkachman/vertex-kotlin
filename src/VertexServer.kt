10c283bcbddf21cd0eac67ec1906c829b4a7674a
Alex Tkachman <alex.tkachman@gmail.com>
2012-07-16 21:22:52 +0300

    more standard way or creating verticle

--- src/VertexServer.kt
+++ src/VertexServer.kt
@@ -2,26 +2,26 @@ package org.vertx.kotlin.core
 
 import org.vertx.java.core.Vertx
 import org.vertx.java.core.http.HttpServer
+import org.vertx.java.deploy.Verticle
+import org.vertx.java.core.http.HttpServerRequest
 
-fun main(args: Array<String>) {
-    val BUFF_SIZE = 32 * 1024
-    val vertx = Vertx.newVertx()!!
-    vertx.createHttpServer{
-        setReceiveBufferSize(BUFF_SIZE)
-        setSendBufferSize(BUFF_SIZE)
-        setAcceptBacklog(32000)
+public class ServerExample() : Verticle() {
 
-        routeMatcher {
-            var k = 0
-            getWithRegEx(".*") {
-                println(k++)
-                end("Hello, World!")
+    public override fun start() {
+        val BUFF_SIZE = 32 * 1024
+        vertx.createHttpServer{
+            setReceiveBufferSize(BUFF_SIZE)
+            setSendBufferSize(BUFF_SIZE)
+            setAcceptBacklog(32000)
+
+            routeMatcher {
+                var k = 0
+                getWithRegEx(".*") {
+                    println(k++)
+                    end("Hello, World!")
+                }
             }
         }
-    }
-    .listen(8080)
-
-    while(true) {
-        Thread.sleep(100.toLong())
+        .listen(8080)
     }
 }
