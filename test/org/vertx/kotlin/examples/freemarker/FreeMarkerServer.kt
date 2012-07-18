e47061ad0b6fa4539cb03b038f62c60912de5ad7
Alex Tkachman <alex.tkachman@gmail.com>
2012-07-18 19:52:53 +0300

    freemarker example

---
+++ test/org/vertx/kotlin/examples/freemarker/FreeMarkerServer.kt
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2012 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.vertx.kotlin.examples.freemarker
+
+import org.vertx.java.core.streams.Pump
+import org.vertx.java.deploy.Verticle
+import org.vertx.kotlin.core.*
+import freemarker.template.Configuration
+import freemarker.cache.FileTemplateLoader
+import java.io.File
+import java.util.HashMap
+import java.util.Date
+import java.io.StringWriter
+import org.vertx.groovy.core.http.HttpServerRequest
+import org.vertx.java.core.json.JsonObject
+import freemarker.template.DefaultObjectWrapper
+import freemarker.template.TemplateModel
+import org.vertx.java.core.json.JsonArray
+import freemarker.template.TemplateHashModel
+import freemarker.template.TemplateScalarModel
+import freemarker.template.TemplateSequenceModel
+
+public class FreeMarkerServer() : Verticle() {
+    class object {
+        val BUFF_SIZE = 32*1024
+    }
+
+    public override fun start() {
+        val fmConfig = Configuration()
+        fmConfig.setObjectWrapper(JsonWrapper())
+
+        createHttpServer{
+            routeMatcher {
+                noMatch {
+                    val model = JsonObject().
+                            putString("now", Date().toString())!!.
+                            putString("message", "Hello, World!")!!.
+                            putArray("list", JsonArray().addString("item 1")!!.addString("item 2")!!.addString("item 3"))
+
+                    val writer = StringWriter()
+                    fmConfig.getTemplate("/test/org/vertx/kotlin/examples/freemarker/index.ftl")!!.process(model, writer)
+                    end(writer.toString() as String)
+                }
+            }
+        }.listen(8080, "localhost")
+    }
+}
+
+class JsonWrapper() : DefaultObjectWrapper(){
+    public override fun wrap(obj: Any?): TemplateModel? {
+        return super<DefaultObjectWrapper>.wrap(when (obj) {
+            is JsonObject -> (obj as JsonObject).toMap()
+
+            is JsonArray -> (obj as JsonArray).toList()
+            else -> obj
+        })
+    }
+}
---
+++ test/org/vertx/kotlin/examples/freemarker/index.ftl
@@ -0,0 +1,13 @@
+<html>
+<head>
+</head>
+<body>
+    <h1>${message}</h1>
+    Now is ${now}
+    <ul>
+        <#list list as item>
+            <li>${item}</li>
+        </#list>
+    </ul>
+</body>
+</html>
